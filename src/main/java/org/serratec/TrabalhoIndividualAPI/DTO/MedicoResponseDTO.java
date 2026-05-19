a43dd2884801b308c4a7aa20006d380001b98d50
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-19 16:45:02 -0300

    Criação DTO Response de Medico

--- src/main/java/org/serratec/TrabalhoIndividualAPI/DTO/MedicoResponseDTO.java
+++ src/main/java/org/serratec/TrabalhoIndividualAPI/DTO/MedicoResponseDTO.java
@@ -1,4 +1,28 @@
 package org.serratec.TrabalhoIndividualAPI.DTO;
 
+import java.util.List;
+
 public class MedicoResponseDTO {
+
+    private Long id;
+    private String nome;
+    private List<EspecialidadeResponseDTO> especialidades;
+
+    public MedicoResponseDTO(List<EspecialidadeResponseDTO> especialidades, Long id, String nome) {
+        this.especialidades = especialidades;
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public List<EspecialidadeResponseDTO> getEspecialidades() {
+        return especialidades;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
