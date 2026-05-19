0141569c370aa035c9b40b85fbbf3c52fa40a681
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-19 16:44:02 -0300

    Criação DTO Response de paciente

--- src/main/java/org/serratec/TrabalhoIndividualAPI/DTO/PacienteResponseDTO.java
+++ src/main/java/org/serratec/TrabalhoIndividualAPI/DTO/PacienteResponseDTO.java
@@ -1,4 +1,33 @@
 package org.serratec.TrabalhoIndividualAPI.DTO;
 
 public class PacienteResponseDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public PacienteResponseDTO(String email, Long id, String nome, String telefone) {
+
+        this.email = email;
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
 }
