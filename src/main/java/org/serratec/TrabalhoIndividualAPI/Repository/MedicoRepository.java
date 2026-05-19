23c51db04347a313bf9364456b0d4ebb94da4de5
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-19 11:32:08 -0300

    Criação do repositório medico

--- src/main/java/org/serratec/TrabalhoIndividualAPI/Repository/MedicoRepository.java
+++ src/main/java/org/serratec/TrabalhoIndividualAPI/Repository/MedicoRepository.java
@@ -1,4 +1,9 @@
 package org.serratec.TrabalhoIndividualAPI.Repository;
 
-public interface MedicoRepository {
+import org.serratec.TrabalhoIndividualAPI.Domain.Medico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
 }
