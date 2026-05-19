e79d6d24e8a0508cad5882b5576522ad1ed9613d
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-19 16:46:12 -0300

    Criação DTO Request de Especialidade

--- src/main/java/org/serratec/TrabalhoIndividualAPI/DTO/EspecialidadeRequestDTO.java
+++ src/main/java/org/serratec/TrabalhoIndividualAPI/DTO/EspecialidadeRequestDTO.java
@@ -1,4 +1,22 @@
 package org.serratec.TrabalhoIndividualAPI.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class EspecialidadeRequestDTO {
+
+    @Size(max = 40, message = "Nome deve ter no máximo 40 caracteres")
+    @NotBlank(message = "O campo Nome não pode estar vazio")
+    private String nome;
+
+    public EspecialidadeRequestDTO() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
