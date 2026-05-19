c01e241902be7055299c147293436cc5015ac0b4
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-19 11:31:51 -0300

    Criação do repositório especialidade

--- src/main/java/org/serratec/TrabalhoIndividualAPI/Repository/EspecialidadeRepository.java
+++ src/main/java/org/serratec/TrabalhoIndividualAPI/Repository/EspecialidadeRepository.java
@@ -1,4 +1,9 @@
 package org.serratec.TrabalhoIndividualAPI.Repository;
 
-public class EspecialidadeRepository {
+import org.serratec.TrabalhoIndividualAPI.Domain.Especialidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long> {
 }
