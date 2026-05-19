044a4807ace3b39f05b8aabc5960f35b8a4378fe
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-19 11:32:21 -0300

    Criação do repositório paciente

--- src/main/java/org/serratec/TrabalhoIndividualAPI/Repository/PacienteRepository.java
+++ src/main/java/org/serratec/TrabalhoIndividualAPI/Repository/PacienteRepository.java
@@ -1,4 +1,9 @@
 package org.serratec.TrabalhoIndividualAPI.Repository;
 
-public class PacienteRepository {
+import org.serratec.TrabalhoIndividualAPI.Domain.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 }
