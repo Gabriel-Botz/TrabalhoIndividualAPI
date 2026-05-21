1583e6f3c86f806792daa137f86129695e9413ee
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-21 15:26:46 -0300

    Adicionando anotações Swagger

---
+++ src/main/java/org/serratec/TrabalhoIndividualAPI/Config/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package org.serratec.TrabalhoIndividualAPI.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI myOpenAPI(){
+
+        Contact contato  = new Contact();
+        contato.setEmail("gabriel.c.botelho@hotmail.com");
+        contato.setName("Gabriel Botelho");
+        contato.setUrl("https://github.com/Gabriel-Botz/TrabalhoIndividualAPI.git");
+
+        License apacheLicense = new License()
+                .name("Apache License 2.0")
+                .url("https://www.apache.org/licenses/LICENSE-2.0/");
+
+
+        Info info = new Info()
+                .title("Clínica Popular")
+                .version("1.0")
+                .contact(contato)
+                .description("Documentação da API do sistema")
+                .termsOfService("")
+                .license(apacheLicense);
+
+        return new OpenAPI().info(info);
+
+    }
+}
