17a2d4c1f8526c3f0f6e2a4264cbd756f5578c54
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-21 01:59:56 -0300

    adicionando lista prontuario

--- src/main/java/org/serratec/TrabalhoIndividualAPI/Repository/ProntuarioRepository.java
+++ src/main/java/org/serratec/TrabalhoIndividualAPI/Repository/ProntuarioRepository.java
@@ -4,6 +4,11 @@ import org.serratec.TrabalhoIndividualAPI.Domain.Prontuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProntuarioRepository extends JpaRepository<Prontuario, Long> {
+
+    List<Prontuario> findByPaciente_Id(Long pacienteId);
+
 }
