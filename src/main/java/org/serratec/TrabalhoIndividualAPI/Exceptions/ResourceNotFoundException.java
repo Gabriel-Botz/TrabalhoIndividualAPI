aea649c8df89016ac5958dc3e4644b752539e3ad
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-21 02:00:28 -0300

    criação ResourceNotFoundException

---
+++ src/main/java/org/serratec/TrabalhoIndividualAPI/Exceptions/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package org.serratec.TrabalhoIndividualAPI.Exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
